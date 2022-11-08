59052b58b9b0934de4929eb98f8256997353e21e
Muhammad-Nasir Shuaib <32900089+greatestnazz@users.noreply.github.com>
2022-11-08 12:48:04 +0100

    Dynamodb instantiated with X-ray

--- backend/src/dataLayer/ToDoAccess.ts
+++ backend/src/dataLayer/ToDoAccess.ts
@@ -1,5 +1,6 @@
 import * as AWS from "aws-sdk";
 //import { DocumentClient } from "aws-sdk/clients/dynamodb";
+const AWSXRay = require('aws-xray-sdk')
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { Types } from 'aws-sdk/clients/s3';
 import { TodoItem } from "../models/TodoItem";
@@ -14,7 +15,7 @@ const XAWS = AWSXRay.captureAWS(AWS)
 
 export class ToDoAccess {
     constructor(
-        private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+        private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
         private readonly s3Client: Types = new XAWS.S3({ signatureVersion: 'v4' }),
         private readonly todoTable = process.env.TODOS_TABLE,
         private readonly s3BucketName = process.env.S3_BUCKET_NAME) {
